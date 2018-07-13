4be3152fac03fbc6d526bc12f6aef69bdf810253
wangjq <wjqhref@163.com>
2018-07-14 00:48:34 +0800

    添加注释

--- src/components/common/common.js
+++ src/components/common/common.js
@@ -15,10 +15,10 @@ class Common {
             'http://t5.tianditu.cn/',
             'http://t6.tianditu.cn/',
             'http://t7.tianditu.cn/',
-            'http://webrd01.is.autonavi.com/appmaptile',// 高德
-            'http://webrd02.is.autonavi.com/appmaptile',
-            'http://webrd03.is.autonavi.com/appmaptile',
-            'http://webrd04.is.autonavi.com/appmaptile',
+            'http://webrd01.is.autonavi.com/',// 高德
+            'http://webrd02.is.autonavi.com/',
+            'http://webrd03.is.autonavi.com/',
+            'http://webrd04.is.autonavi.com/',
         ];
     }
 
@@ -33,7 +33,7 @@ class Common {
         // 如果本地没有API就使用这行,url 为空则默认加载官网API;
         // return loadModules (modules, this.options (''));
         // 如果本地有部署API,则使用这句,根据情况修改自己的地址;
-        return loadModules (modules, this.options ('http://localhost:8080/4.7/dojo/dojo.js'));
+        return loadModules (modules, this.options ('http://localhost:8080/4.8/dojo/dojo.js'));
     }
 
     _maptype() {
@@ -64,12 +64,12 @@ class Common {
                 // 通过传入参数 level,row,col 来获得 加载瓦片URL;
                 getTileUrl(level, row, col) {
                     // 高德地图
-                    return 'http://webrd0' + (col % 4 + 1) + '.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x=' + col + '&y=' + row + '&z=' + level;
+                    /*return 'http://webrd0' + (col % 4 + 1) + '.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x=' + col + '&y=' + row + '&z=' + level;*/
                     // 天地图
-                    /* return 'http://t' + col % 8 + '.tianditu.cn/' + self._maptype () + '_c/wmts?' +
+                     return 'http://t' + col % 8 + '.tianditu.cn/' + self._maptype () + '_c/wmts?' +
                          'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=' + self._maptype () +
                          '&STYLE=default&TILEMATRIXSET=c&TILEMATRIX=' +
-                         level + '&TILEROW=' + row + '&TILECOL=' + col + '&FORMAT=tiles';*/
+                         level + '&TILEROW=' + row + '&TILECOL=' + col + '&FORMAT=tiles';
                 },
                 // 此方法获取指定级别和大小的切片。重写此方法以处理从服务器返回的数据。
                 fetchTile(level, row, col) {
