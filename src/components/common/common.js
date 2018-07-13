48236a2ec4c28efe515a3c5e953a60009c26a000
wangjq <wjqhref@163.com>
2018-07-13 17:57:36 +0800

    requestConfig 优化

--- src/components/common/common.js
+++ src/components/common/common.js
@@ -5,7 +5,7 @@ import { loadModules } from 'esri-loader';
 
 // 该类主要包含初始化自定义底图,并将底图加载到页面上 ;
 class Common {
-    constructor () {
+    constructor() {
         this.serverAddress = [
             'http://t0.tianditu.cn/', // 天地图
             'http://t1.tianditu.cn/',
@@ -23,21 +23,22 @@ class Common {
     }
 
     // 模块加载地址
-    options (url) {
+    options(url) {
         return {
             url: url || '' // 存在着是用户自定义的地址,为空则从 arcgis 官网加载
         };
     }
 
-    commonLoad (modules, options) {
-        return loadModules (modules, options);
+    commonLoad(modules) {
+        return loadModules (modules, this.options (''));
+        // return loadModules (modules, this.options('http://localhost:8080/4.7/dojo/dojo.js'));
     }
 
-    _maptype () {
+    _maptype() {
         return 'vec';
     }
 
-    loadBaseTitle () {
+    loadBaseTitle() {
         let self = this;
         this.commonLoad ([
             'esri/layers/BaseTileLayer',
@@ -57,18 +58,16 @@ class Common {
                         type: Color
                     }
                 },
-
                 // generate the tile url for a given level, row and column
                 getTileUrl: function (level, row, col) {
                     // 高德地图
                     return 'http://webrd0' + (col % 4 + 1) + '.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x=' + col + '&y=' + row + '&z=' + level;
                     // 天地图
-                   /* return 'http://t' + col % 8 + '.tianditu.cn/' + self._maptype () + '_c/wmts?' +
-                        'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=' + self._maptype () +
-                        '&STYLE=default&TILEMATRIXSET=c&TILEMATRIX=' +
-                        level + '&TILEROW=' + row + '&TILECOL=' + col + '&FORMAT=tiles';*/
+                    /* return 'http://t' + col % 8 + '.tianditu.cn/' + self._maptype () + '_c/wmts?' +
+                         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=' + self._maptype () +
+                         '&STYLE=default&TILEMATRIXSET=c&TILEMATRIX=' +
+                         level + '&TILEROW=' + row + '&TILECOL=' + col + '&FORMAT=tiles';*/
                 },
-
                 // This method fetches tiles for the specified level and size.
                 // Override this method to process the data returned from the server.
                 fetchTile: function (level, row, col) {
@@ -76,7 +75,6 @@ class Common {
                     // call getTileUrl() method to construct the URL to tiles
                     // for a given level, row and col provided by the LayerView
                     let url = this.getTileUrl (level, row, col);
-
                     // request for tiles based on the generated url
                     // set allowImageDataAccess to true to allow
                     // cross-domain access to create WebGL textures for 3D.
@@ -88,18 +86,15 @@ class Common {
                         // when esri request resolves successfully
                         // get the image from the response
                         let image = response.data;
-                        let width = this.tileInfo.size[ 0 ];
-                        let height = this.tileInfo.size[ 0 ];
-
+                        let width = this.tileInfo.size[0];
+                        let height = this.tileInfo.size[0];
                         // create a canvas with 2D rendering context
                         let canvas = document.createElement ('canvas');
                         let context = canvas.getContext ('2d');
                         canvas.width = width;
                         canvas.height = height;
-
                         // Draw the blended image onto the canvas.
                         context.drawImage (image, 0, 0, width, height);
-
                         return canvas;
                     }.bind (this));
                 }
@@ -113,16 +108,15 @@ class Common {
         });
     }
 
-
-    init () {
+    init() {
         this.requestConfig ();
         this.commonLoad (
-            [ 'esri/Map', 'esri/views/MapView', 'esri/config' ],
+            ['esri/Map', 'esri/views/MapView', 'esri/config'],
             this.options ('')
         )
-        .then (([ Map ]) => {
+        .then (([Map]) => {
             let map = new Map ({
-                layers: [ this.stamenTileLayer ]
+                layers: [this.stamenTileLayer]
             });
             // and we show that map in a container w/ id #viewDiv
             this.webmap = map;
@@ -134,9 +128,9 @@ class Common {
         });
     }
 
-    initMapView () {
-        this.commonLoad ([ 'esri/views/MapView' ])
-        .then (([ MapView ]) => {
+    initMapView() {
+        this.commonLoad (['esri/views/MapView'])
+        .then (([MapView]) => {
             let view = new MapView ({
                 map: this.webmap,
                 container: 'viewDiv',
@@ -147,12 +141,12 @@ class Common {
         });
     }
 
-    requestConfig () {
-        this.commonLoad ([ 'esri/config' ])
-        .then (([ esriConfig ]) => {
-            for ( let val of this.serverAddress ) {
-                esriConfig.request.corsEnabledServers.push (val);
-            }
+    requestConfig() {
+        this.commonLoad (['esri/config'])
+        .then (([esriConfig]) => {
+            let servers = esriConfig.request.corsEnabledServers;
+            servers = servers.concat (this.serverAddress);
+            esriConfig.request.corsEnabledServers = servers;
         }, reason => {
             console.error (reason);
         });
